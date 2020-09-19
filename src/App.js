cfb6b347ed2105edf7c4a8102b95bec63816c845
alexa2001 <amlagunas70@gmail.com>
2020-09-19 12:32:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './Components/Routes';
+import Navbar from './Components/Navbar';
+import './Styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><strong>{'\u2203'}</strong>NGAUGE</h1>
+        <Navbar />
       </header>
+      <div className="headblock headblock3" />
+      <div className="headblock headblock2" />
+      <div className="headblock headblock1" />
+      <Routes />
     </div>
-  );
+  )
 }
+
 
 export default App;
